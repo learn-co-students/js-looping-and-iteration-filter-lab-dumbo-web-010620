7602c73a0306f67a0553c0d364e65519915e2d89
Julianna Boyd <julianna.boyd91@gmail.com>
2020-02-18 14:55:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching (alldrivers, name) {
+    return alldrivers.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+      });
+    }
+    
+    function fuzzyMatch (alldrivers, partialName) {
+      let lengthOfName = partialName.length;
+      return alldrivers.filter(function (driverName) {
+        return driverName.slice(0, lengthOfName) === partialName;
+      });
+    }
+    
+    function matchName (alldrivers, name) {
+      return alldrivers.filter(function (driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+      });
+    }
